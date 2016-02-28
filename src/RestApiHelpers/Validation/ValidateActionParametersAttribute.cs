5e831c3b1fff2e3f8743c4bf7ce0b455d2b45a17
Mark Vincze <mvincze@travix.com>
2016-02-28 20:23:05 +0100

    Add ValidateActionParametersAttribute

---
+++ src/RestApiHelpers/Validation/ValidateActionParametersAttribute.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNet.Mvc.Controllers;
+using Microsoft.AspNet.Mvc.Filters;
+using Microsoft.AspNet.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace RestApiHelpers.Validation
+{
+    public class ValidateActionParametersAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var descriptor = context.ActionDescriptor as ControllerActionDescriptor;
+
+            if (descriptor != null)
+            {
+                var parameters = descriptor.MethodInfo.GetParameters();
+
+                foreach (var parameter in parameters)
+                {
+                    var argument = context.ActionArguments[parameter.Name];
+
+                    EvaluateValidationAttributes(parameter, argument, context.ModelState);
+                }
+            }
+
+            base.OnActionExecuting(context);
+        }
+
+        private void EvaluateValidationAttributes(ParameterInfo parameter, object argument, ModelStateDictionary modelState)
+        {
+            var validationAttributes = parameter.CustomAttributes;
+
+            foreach (var attributeData in validationAttributes)
+            {
+                var attributeInstance = CustomAttributeExtensions.GetCustomAttribute(parameter, attributeData.AttributeType);
+
+                var validationAttribute = attributeInstance as ValidationAttribute;
+
+                if (validationAttribute != null)
+                {
+                    var isValid = validationAttribute.IsValid(argument);
+                    if (!isValid)
+                    {
+                        modelState.AddModelError(parameter.Name, validationAttribute.FormatErrorMessage(parameter.Name));
+                    }
+                }
+            }
+        }
+    }
+}
