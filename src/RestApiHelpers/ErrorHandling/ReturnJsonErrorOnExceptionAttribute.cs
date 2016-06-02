54de45ded32ccd7daeabbdc12c4f7c4f2bdde768
Mark Vincze <mvincze@travix.com>
2016-06-02 17:16:30 +0200

    Add filter for returning json errors

---
+++ src/RestApiHelpers/ErrorHandling/ReturnJsonErrorOnExceptionAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace RestApiHelpers.ErrorHandling
+{
+    public class ReturnJsonErrorOnExceptionAttribute : ExceptionFilterAttribute
+    {
+        private readonly ILogger<ReturnJsonErrorOnExceptionAttribute> logger;
+
+        public ReturnJsonErrorOnExceptionAttribute(ILogger<ReturnJsonErrorOnExceptionAttribute> logger)
+        {
+            this.logger = logger;
+        }
+
+        public override void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+
+            logger.LogError($"An unhandled exception was thrown by the application.{Environment.NewLine}{exception.ToString()}");
+
+            context.HttpContext.Response.StatusCode = 500;
+
+            context.Result = new JsonResult(
+                new
+                {
+                    errors = new[]
+                        {
+                            new
+                            {
+                                message = exception.Message,
+                                details = exception.ToString()
+                            }
+                        }
+                });
+        }
+    }
+}
