05eb4f87a040511bc5befbec0ea9db1520998ec1
Mark Vincze <mvincze@travix.com>
2016-06-02 16:36:12 +0200

    Implement a simple request-response logger middleware

---
+++ src/RestApiHelpers/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using RestApiHelpers.Logging;
+
+namespace RestApiHelpers.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void UseSimpleRequestResponseLogging(this IApplicationBuilder builder, params string[] routesToIgnore)
+        {
+            SimpleRequestResponseLoggerMiddleware.SetRoutesToIgnore(routesToIgnore);
+
+            builder.UseMiddleware<SimpleRequestResponseLoggerMiddleware>();
+        }
+    }
+}
---
+++ src/RestApiHelpers/Logging/SimpleRequestResponseLoggerMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace RestApiHelpers.Logging
+{
+    public class SimpleRequestResponseLoggerMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<SimpleRequestResponseLoggerMiddleware> logger;
+
+        private static string[] routesToIgnore;
+
+        public SimpleRequestResponseLoggerMiddleware(RequestDelegate next, ILogger<SimpleRequestResponseLoggerMiddleware> logger)
+        {
+            this.next = next;
+            this.logger = logger;
+        }
+
+        public static void SetRoutesToIgnore(params string[] routesToIgnore)
+        {
+            SimpleRequestResponseLoggerMiddleware.routesToIgnore = routesToIgnore;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var doLogging = routesToIgnore == null || !routesToIgnore.Any(r => context.Request.Path.StartsWithSegments(r));
+
+            if (doLogging)
+            {
+                logger.LogInformation($"Request starting {context.Request.Method} {context.Request.GetDisplayUrl()}");
+            }
+
+            var stopwatch = Stopwatch.StartNew();
+
+            await next.Invoke(context);
+
+            if (doLogging)
+            {
+                stopwatch.Stop();
+
+                logger.LogInformation($"Request finished in {stopwatch.ElapsedMilliseconds}ms {context.Response.StatusCode} {context.Response.ContentType}");
+            }
+        }
+    }
+}
